e8c19c57f55ce3f937163980b4de711f237be010
RichardSellies <richard.sellies@gmail.com>
2024-02-12 18:40:43 +1100

    1
    1

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ $(document).ready(function(){
       $(this).prev(".cursor").hide();
     });
   });
+
+
+
+  $(document).ready(function () { 
+    // On button click, get value 
+    // of input control 
+    $("#getValBtnID").click(function () { 
+      let inputContentStart = $("#feelingInput").text(); 
+      let inputStyleInput = $("#styleInput").text(); 
+      console.log(inputContentStart); 
+      console.log(inputStyleInput); 
+      console.log("click");
+    });
+}); 
+
+
